aed80e84c775153d0ec06e45436175cf4015449a
Yinlerens <tpc1919561288@gmail.com>
2025-05-29 15:09:55 +0800

    chore: 删除不再需要的迁移文件和更新Prisma模式

--- front/prisma/migrations/20250514064233_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "VerificationToken_identifier_token_key";
-
--- DropIndex
-DROP INDEX "VerificationToken_token_key";
-
--- AlterTable
-ALTER TABLE "VerificationToken" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
